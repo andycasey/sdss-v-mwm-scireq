1e1808fcda5d858f201321b520c7d941e84ba7b0
Andy Casey <andycasey@gmail.com>
2018-01-03 11:23:23 +1100

    add download script

---
+++ scripts/download-apogee-dr14.sh
@@ -0,0 +1,12 @@
+# from http://www.sdss.org/dr14/data_access/bulk/
+wget https://data.sdss.org/sas/dr14/apogee/spectro/redux/r8/allVisit-l31c.2.fits
+wget https://data.sdss.org/sas/dr14/apogee/spectro/redux/r8/allStar-l31c.2.fits
+rsync -aLvz --include "[0-9][0-9][0-9][0-9]/" \
+    --include "apStar-*[0-9][0-9][0-9][0-9][0-9][0-9][0-9].fits" --exclude "*"\
+    --prune-empty-dirs --progress \
+    rsync://data.sdss.org/dr14/apogee/spectro/redux/r8/stars/apo25m/ stars/apo25m/
+rsync -aLvz --include "[0-9][0-9][0-9][0-9]/" \
+    --include "apStar-*[0-9][0-9][0-9][0-9][0-9][0-9][0-9].fits" --exclude "*"\
+    --prune-empty-dirs --progress \
+    rsync://data.sdss.org/dr14/apogee/spectro/redux/r8/stars/apo1m/ stars/apo1m/
+
