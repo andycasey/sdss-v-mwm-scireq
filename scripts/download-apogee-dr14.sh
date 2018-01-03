d946c8e035f0d6340e25e41be8be83fdc00c8992
Ubuntu <ubuntu@sdss.novalocal>
2018-01-03 00:25:58 +0000

    update

--- scripts/download-apogee-dr14.sh
+++ scripts/download-apogee-dr14.sh
@@ -1,4 +1,7 @@
 # from http://www.sdss.org/dr14/data_access/bulk/
+mkdir -p dr14/apogee/spectro/redux/r8/stars/
+cd dr14/apogee/spectro/redux/r8/stars/
+mkdir apo25m apo1m
 wget https://data.sdss.org/sas/dr14/apogee/spectro/redux/r8/allVisit-l31c.2.fits
 wget https://data.sdss.org/sas/dr14/apogee/spectro/redux/r8/allStar-l31c.2.fits
 rsync -aLvz --include "[0-9][0-9][0-9][0-9]/" \
@@ -9,4 +12,4 @@ rsync -aLvz --include "[0-9][0-9][0-9][0-9]/" \
     --include "apStar-*[0-9][0-9][0-9][0-9][0-9][0-9][0-9].fits" --exclude "*"\
     --prune-empty-dirs --progress \
     rsync://data.sdss.org/dr14/apogee/spectro/redux/r8/stars/apo1m/ stars/apo1m/
-
+cd ../../../../../../
