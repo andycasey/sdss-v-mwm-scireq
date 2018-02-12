877cb763edc40312a84f4291b15c5cc58a5402c0
Andy Casey <andycasey@gmail.com>
2018-02-13 02:14:33 +1100

    update 6

--- exp_6.py
+++ exp_6.py
@@ -20,7 +20,7 @@ import thecannon.restricted
 
 from apogee import config
 
-from experiments import get_balanced_training_set, precision_from_repeat_visits
+from experiments import get_balanced_training_set, precision_from_repeat_calibration_visits
 from utils import atomic_number
 label_names = ["TEFF", "LOGG", "FE_H", "C_FE", "N_FE", "O_FE", "NA_FE",
                "MG_FE", "AL_FE", "SI_FE", "P_FE", "S_FE", "K_FE", "CA_FE",
@@ -33,7 +33,7 @@ vacuum_wavelengths, training_set_labels, training_set_flux, training_set_ivar \
 
 
 # ----------- #
-train_kwds = dict(op_kwds=dict(factr=1e12, pgtol=1e-5))
+train_kwds = dict()
 test_kwds = dict()
 OUTPUT_PATH = "experiments/6/"
 # ----------- #
@@ -41,6 +41,7 @@ OUTPUT_PATH = "experiments/6/"
 if not os.path.exists(OUTPUT_PATH): os.mkdir(OUTPUT_PATH)
 
 censors = {}
+weights = {}
 for label_name in label_names:
     if "_" not in label_name: continue
 
@@ -52,50 +53,49 @@ for label_name in label_names:
         "dr14/apogee/windows",
         "{}_DR13_Window_Ranges_VAC.csv".format(element.title()))
 
-    wavelengths = np.loadtxt(path, delimiter=",", usecols=(0, ))
-
-    indices = np.searchsorted(vacuum_wavelengths, wavelengths)
-
-    raise a
-
-raise a
-
-transitions = linelist.get_linelist()
-
-window = 0.5 # Angstroms surrounding each line.
-censors = {}
-for label_name in label_names:
-    if "_" not in label_name: continue
-
-    element = label_name.split("_")[0]
-
-    Z = atomic_number(element)
+    data = np.loadtxt(path, delimiter=",", usecols=(0, 1))
 
     censors[label_name] = np.zeros(vacuum_wavelengths.size, dtype=bool)
+    weights[label_name] = np.zeros(vacuum_wavelengths.size, dtype=float)
 
-    match = (transitions["specid"].astype(int) == Z)
-    for wavelength in transitions["lam"][match]:
-        wl = wavelength * 10 # to angstroms.
-        censors[label_name] += ((wl + window) >= vacuum_wavelengths) * (vacuum_wavelengths >= (wl - window))
-
-    censors[label_name] = censors[label_name]
+    indices = np.searchsorted(vacuum_wavelengths, data.T[0])
+    censors[label_name][indices] = True
+    weights[label_name][indices] = data.T[1]
 
 
-vectorizer = tc.vectorizer.PolynomialVectorizer(label_names, order=2)
-# Restrict the vectorizer to only use cross-terms with TEFF and LOGG:
-new_terms = \
-    [t for t in vectorizer.get_human_readable_label_vector().split(" + ")[1:]
-        if "*" not in t or ("TEFF" in t.split("*") or "LOGG" in t.split("*"))]
-vectorizer = tc.vectorizer.PolynomialVectorizer(terms=new_terms)
+
+model_path = os.path.join(OUTPUT_PATH, "aspcap_censored.model")
+if os.path.exists(model_path):
+    model = tc.CannonModel.read(model_path)
+
+else:
+
+    vectorizer = tc.vectorizer.PolynomialVectorizer(label_names, order=2)
+
+    """
+    # Restrict the vectorizer to only use cross-terms with TEFF and LOGG:
+    new_terms = \
+        [t for t in vectorizer.get_human_readable_label_vector().split(" + ")[1:]
+            if "*" not in t or ("TEFF" in t.split("*") or "LOGG" in t.split("*"))]
+    vectorizer = tc.vectorizer.PolynomialVectorizer(terms=new_terms)
 
 
-model = tc.restricted.RestrictedCannonModel(
-    training_set_labels, training_set_flux, training_set_ivar, vectorizer,
-    dispersion=vacuum_wavelengths, censors=censors,
-    theta_bounds=dict([(ln, (None, 0)) for ln in label_names if ln.endswith("_FE")]))
+    model = tc.restricted.RestrictedCannonModel(
+        training_set_labels, training_set_flux, training_set_ivar, vectorizer,
+        dispersion=vacuum_wavelengths, censors=censors,
+        theta_bounds=dict([(ln, (None, 0)) for ln in label_names if ln.endswith("_FE")]))
+    """
 
-model.train(**train_kwds)
-model.write(os.path.join(OUTPUT_PATH, "restricted_wo_ct_censored.model"), overwrite=True)
+
+
+    model = tc.CannonModel(training_set_labels, training_set_flux, training_set_ivar,
+        vectorizer, dispersion=vacuum_wavelengths, censors=censors)
+    model.train(**train_kwds)
+
+    # Some pixels had issues. Re-train using current coefficients as initial values.
+    model.train(op_method="powell")
+
+    model.write(model_path,overwrite=True)
 
 
 # Plot theta.
@@ -103,7 +103,93 @@ fig = tc.plot.theta(model, indices=np.arange(len(label_names) + 1))
 fig.set_figheight(20)
 fig.tight_layout()
 fig.subplots_adjust(hspace=0, wspace=0)
-fig.savefig(os.path.join(OUTPUT_PATH, "baseline_wo_ct_censored_theta.pdf"), dpi=300)
+fig.savefig(
+    os.path.join(OUTPUT_PATH, "aspcap_censored_theta.pdf"),
+    dpi=300)
+
+
+# Plot the weights compared to the spectral derivatives?????
+fig, axes = plt.subplots(4, 5, figsize=(16, 20))
+axes = np.array(axes).flatten()
+for ax, label_name in zip(axes, label_names):
+
+    ax.text(0.9, 0.9, label_name, 
+        transform=ax.transAxes, horizontalalignment="right")
+    
+    if "_" not in label_name: continue
+
+    theta_indices = model.theta.T[1 + model.vectorizer.label_names.index(label_name)]
+    pixel_indices = weights[label_name] > 0
+
+    ax.scatter(
+        weights[label_name][pixel_indices],
+        theta_indices[pixel_indices],
+        s=1, c="#000000")
+    ax.set_xlabel(r"weight")
+    ax.set_ylabel(r"$\theta$")
+
+fig.tight_layout()
+fig.savefig(
+    os.path.join(OUTPUT_PATH, "aspcap_censored_weights-vs-theta.pdf"),
+    dpi=300)
+
+
+#visit_snr, combined_snr, visit_snr_labels, combined_snr_labels, apogee_ids \
+results_path = os.path.join(OUTPUT_PATH, "aspcap_censored_6.pkl")
+if os.path.exists(results_path):
+    with open(results_path, "rb") as fp:
+        results = pickle.load(fp)
+
+else:
+    results = precision_from_repeat_calibration_visits(model, N_comparisons=10000)
+    with open(results_path, "wb") as fp:
+        pickle.dump(results, fp)
+
+
+raise a
+
+
+
+model = tc.restricted.CannonModel(
+    training_set_labels, training_set_flux, training_set_ivar, vectorizer, 
+    dispersion=vacuum_wavelengths, censors=censors)
+
+model.train(**train_kwds)
+model.write(
+    os.path.join(OUTPUT_PATH, "aspcap_censored_restricted.model"),
+    overwrite=True)
+
+
+
+# Plot the weights compared to the spectral derivatives?????
+fig, axes = plt.subplots(4, 5, figsize=(16, 20))
+axes = np.array(axes).flatten()
+for ax, label_name in zip(axes, label_names):
+    if "_" not in label_name: continue
+
+    theta_indices = model.theta.T[1 + model.vectorizer.label_names.index(label_name)]
+    pixel_indices = weights[label_name] > 0
+
+    ax.scatter(
+        weights[label_name][pixel_indices],
+        theta_indices[pixel_indices],
+        s=1, c="#000000")
+    ax.text(0.9, 0.9, label_name, transform=ax.transAxes)
+    ax.set_xlabel(r"weight")
+    ax.set_ylabel(r"$\theta$")
+
+fig.tight_layout()
+fig.savefig(
+    os.path.join(OUTPUT_PATH, "aspcap_censored_restricted_weights-vs-theta.pdf"),
+    dpi=300)
+
+
+
+
+
+
+raise a
+
 
 
 # Do one-to-one.
@@ -111,13 +197,14 @@ oto_labels, oto_cov, oto_meta = model.test(training_set_flux, training_set_ivar,
                                            **test_kwds)
 
 fig = tc.plot.one_to_one(model, oto_labels)
-fig.savefig(os.path.join(OUTPUT_PATH, "baseline_wo_ct_censored_one_to_one.pdf"))
+fig.savefig(
+    os.path.join(OUTPUT_PATH, "aspcap_censored_restricted_noct_oto.pdf"),
+    dpi=300)
 
 
-# Run the model on all visits and plot dispersion as a function of S/N value.
-snr, combined_snr, label_difference, filename = precision_from_repeat_visits(
-    model, N_comparisons=1000, test_kwds=test_kwds)
-
-with open(os.path.join(OUTPUT_PATH, "baseline_wo_ct_censored_precision_snr.pkl"), "wb") as fp:
-    pickle.dump((snr, combined_snr, label_difference, filename), fp)
+# Run the model on individual visits from the calibration set.
+#visit_snr, combined_snr, visit_snr_labels, combined_snr_labels, apogee_ids \
+results = precision_from_repeat_calibration_visits(model, N_comparisons=10000)
+with open(os.path.join(OUTPUT_PATH, "calibration_precision_6.pkl"), "wb") as fp:
+    pickle.dump(results, fp)
 
